c0d206899a7824982720322db9051533f32d52dc
raedchaker <raedchaker99@gmail.com>
2021-12-20 15:06:35 +0100

    room page shows old messages

--- src/app/chat/room/room.component.ts
+++ src/app/chat/room/room.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Message } from 'src/app/_models/message';
 import { Room } from 'src/app/_models/room';
+import { ChatService } from 'src/app/_services/chat.service';
 import { RoomService } from 'src/app/_services/room.service';
 
 @Component({
@@ -11,12 +13,17 @@ import { RoomService } from 'src/app/_services/room.service';
 export class RoomComponent implements OnInit {
 
   room: Room = new Room();
-  constructor(private roomService: RoomService, private route: ActivatedRoute, private router: Router) { }
+  messages: Message[] = [];
+  constructor(private chatService: ChatService,private roomService: RoomService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.roomService.getRoomById(params.id).subscribe(res => {
         this.room = res;
+      });
+      this.chatService.getChatByRoomId(params.id).subscribe(res => {
+        this.messages = res;
+        console.log(res);
       });
     });
   }
